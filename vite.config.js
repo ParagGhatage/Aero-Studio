d78884cb7ee50c9d2678a6dc7b2583262d3a6c71
Parag Ghatage <parag.dev2025@gmail.com>
2026-06-08 02:48:38 +0530

    Progressive Web App config added

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,35 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Aero Studio',
+        short_name: 'AeroStudio',
+        description: 'Local-first media workspace',
+        theme_color: '#0D0D0D',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
